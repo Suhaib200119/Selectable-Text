0f52577d3b8a9f15aa67d3194ee680f4214a2095
Suhaib200119 <sis.22.19a@gmail.com>
2021-08-10 15:31:23 +0300

    Selectable Text

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Selectable Text"),
+      ),
+      body: Center(
+        child: SelectableText(
+          "This is selectable Text widget in flutter",
+          showCursor: true,
+          toolbarOptions: ToolbarOptions(
+            copy: true,
+            selectAll: true,
+          ),
+        ),
+      ),
+    );
+  }
+}
